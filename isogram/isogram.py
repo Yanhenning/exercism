2af2ee18f019a76038c6fcafd7cba7c58cea1d94
yanhenning <yanhenning@gmail.com>
2021-05-28 17:29:24 -0300

    look for letters instead of looking by not letters

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,3 +1,3 @@
 def is_isogram(string):
-    cleaned_string = string.lower().replace(',', '').replace('-', '').replace(' ', '')
+    cleaned_string = ''.join([char for char in string.lower() if char.isalpha()])
     return len(cleaned_string) == len(set(cleaned_string))
